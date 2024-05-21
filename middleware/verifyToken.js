28616e46ca2124ac73fe535349c1e0b4060e0295
nurulafani03 <nurulafani03@gmail.com>
2024-05-22 02:02:11 +0700

    token

---
+++ middleware/verifyToken.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { secret } = require('../config/config'); // Import secret key dari config
+
+function verifyToken(req, res, next) {
+  const token = req.headers['authorization']; // Ambil token dari header authorization
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret); // Verify token dengan secret key
+    req.user = decoded; // Simpan informasi user yang didecode di req.user
+    next(); // Lanjutkan ke route berikutnya
+  } catch (error) {
+    return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+  }
+}
+
+module.exports = verifyToken;
